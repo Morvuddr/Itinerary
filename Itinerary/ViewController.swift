053ca360b48f00896f0e12bc49a16f4d73fcf0f6
Igor Bopp <igor_bopp@mail.ru>
2019-02-17 12:27:42 +0300

    Initial Commit

---
+++ Itinerary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Itinerary
+//
+//  Created by Игорь Бопп on 17/02/2019.
+//  Copyright © 2019 Igor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
